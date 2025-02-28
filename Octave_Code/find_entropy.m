c25155d06837ece3b0846e3a0f2b1d78a039cfe9
William Clampitt <wac018@shsu.edu>
2025-02-28 10:29:14 -0600

    Cleaned up intermediate build files

--- Octave_Code/find_entropy.m
+++ Octave_Code/find_entropy.m
@@ -1,5 +1,5 @@
-t = 1;
-N = 10;
+t = 2;
+N = 20;
 
 R1 = [[-1, 0, 0]; [2*t, 1, 0]; [2/t, 0, 1]];
 
@@ -9,8 +9,8 @@ ls = [L_t(M{1})];
 
 for p = [0:N]
     row_length_cell = get_next_row(t, p, M);
-    filename_row = ["row_", num2str(p), ".txt"];
-    filename_lengths = ["lengths_row_", num2str(p), ".csv"];
+    filename_row = ["row_t_", num2str(p),"_", num2str(p), ".txt"];
+    filename_lengths = ["lengths_row_t_", num2str(p),"_", num2str(p), ".csv"];
 
     M = row_length_cell{1};
     new_ls = row_length_cell{2};
@@ -18,5 +18,5 @@ for p = [0:N]
     save("-text", filename_row, "M");
     csvwrite(filename_lengths, new_ls);
     ls = [ls, new_ls];
-
+    disp(["finished row: ", num2str(p)]);
 endfor
